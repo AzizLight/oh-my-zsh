4bc283dcf5a2bbcbb5c0ce6f326a17f97ef1094a
Aziz Light <aziiz.light@gmail.com>
2010-09-07 15:45:52 +0200

    Changed the terminal windows name to (just) the current process.

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -1,10 +1,11 @@
 case "$TERM" in
   xterm*|rxvt*)
+    # Title bar contains either the pwd or the current job
     preexec () {
-      print -Pn "\e]0;%n@%m: $1\a"  # xterm
+      print -Pn "\e]0;$1\a"  # xterm
     }
     precmd () {
-      print -Pn "\e]0;%n@%m: %~\a"  # xterm
+      print -Pn "\e]0;%~\a"  # xterm
     }
     ;;
   screen*)
