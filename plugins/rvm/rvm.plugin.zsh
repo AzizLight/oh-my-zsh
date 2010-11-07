a415ff4d990d69429c7e86f7fbd4f0783b65081e
Aziz Light <aziiz.light@gmail.com>
2010-11-07 18:09:48 +0100

    Added a function to show the current ruby name, version and gemset in the rvm plugin.

--- plugins/rvm/rvm.plugin.zsh
+++ plugins/rvm/rvm.plugin.zsh
@@ -12,3 +12,17 @@ alias r187="rvm use 1.8.7"
 alias rd="rvm use default"
 alias rg="rvm gemset"
 alias rgl="rvm gemset list"
+
+# get the ruby name and version as well as the gemset
+# from https://github.com/jtzemp/oh-my-zsh/blob/deb452e823876e26bc91947fdd55f75873b02c1d/lib/rvm.zsh#L9
+rvg() {
+  local gemset=$(echo $GEM_HOME | awk -F'@' '{print $2}')
+  [ "$gemset" != "" ] && gemset="$fg[blue]@$fg[green]$gemset$fg[default]" # <- The @ sign is here
+  
+  # local version=$(echo $MY_RUBY_HOME | awk -F'-' '{print $2}')
+  local version=$(~/.rvm/bin/rvm-prompt s i v)
+  [ "$version" != "" ] && version="$fg[red]$version$fg[default]"
+  
+  local full="$version$gemset"
+  [ "$full" != "" ] && echo "$full"
+}
