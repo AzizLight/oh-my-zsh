aedc46bd3f207b9984e12e619ada46352af40234
Aziz Light <aziiz.light@gmail.com>
2010-11-23 14:33:22 +0100

    Added two new git aliases to the git plugin

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -14,6 +14,7 @@ alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
 alias glg='git log --stat --max-count=5'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias ggl="git log --max-count=20 --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
@@ -31,3 +32,8 @@ function current_branch() {
 alias ggpull='git pull origin $(current_branch)'
 alias ggpush='git push origin $(current_branch)'
 alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
+
+# clone what's in the clipboard
+# Works only on OS X
+# TODO: the alias sucks ass, rename it to something better!
+alias gclp="git clone $(pbpaste)"
