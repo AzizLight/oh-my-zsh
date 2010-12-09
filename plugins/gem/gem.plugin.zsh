ae82d8cf7c26244cd4c884166d50054c5e3f5532
Aziz Light <aziiz.light+github@gmail.com>
2010-12-09 17:21:45 +0100

    I missed the m

--- plugins/gem/gem.plugin.zsh
+++ plugins/gem/gem.plugin.zsh
@@ -3,6 +3,6 @@ fpath=($ZSH/plugins/gem $fpath)
 autoload -U compinit
 compinit -i
 
-alias gei="gem install"
-alias gel="gem list"
-alias ges="gem search --remote"
+alias gemi="gem install"
+alias geml="gem list"
+alias gems="gem search --remote"
