39c9cb896e3d5df8dae8ed6fb781a085c95cb3a2
Aziz Light <aziiz.light+github@gmail.com>
2011-01-30 20:16:43 +0100

    Added a vim plugin

---
+++ custom/plugins/vim/vim.plugin.zsh
@@ -0,0 +1,8 @@
+alias v="mvim"
+alias vz="v ~/.oh-my-zsh/"
+alias vrc="v ~/.zshrc"
+alias vzrc="v ~/.oh-my-zsh/ ~/.zshrc"
+alias vv="v ~/.{,g}vimrc.local"
+alias vvv="v ~/.{,g}vimrc{,.local}"
+
+alias conque="export az_prompt_type='simple' ; s ; clear"
