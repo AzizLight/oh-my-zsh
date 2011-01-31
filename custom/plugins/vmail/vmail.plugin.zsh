cd64306468d6b79f219655cd3a4585f1609b990e
Aziz Light <aziiz.light+github@gmail.com>
2011-01-31 18:22:00 +0100

    Customized the vm vmail function to suit my needs

--- custom/plugins/vmail/vmail.plugin.zsh
+++ custom/plugins/vmail/vmail.plugin.zsh
@@ -2,11 +2,19 @@
 # vmail using one command.
 # Taken from: https://gist.github.com/743626
 vm() {
-  if [[ $1 = "p" ]]; then
-    rvm system exec vmail -c ~/.vmailrc-p
-  elif [[ $1 = "w" ]]; then
-    rvm system exec vmail -c ~/.vmailrc-w
+  list_of_emails="/Users/aziz/Dropbox/Private/list_of_emails"
+  if [[ $1 = "r" ]]; then
+    rvm 1.9.2 exec vmail -c ~/.vmailrc-rob
+  elif [[ $1 = "z" ]]; then
+    rvm 1.9.2 exec vmail -c ~/.vmailrc-aziz
+  elif [[ $1 = "y" ]]; then
+    rvm 1.9.2 exec vmail -c ~/.vmailrc-youjiii
+  elif [[ $1 = "l" ]]; then
+    rvm 1.9.2 exec vmail -c ~/.vmailrc-leeloo
+  elif [[ $1 = "rsub" ]]; then
+    rvm 1.9.2 exec vmail -c ~/.vmailrc-robsub
   else
     echo "Please specify an account."
+    cat list_of_emails
   fi
 }
