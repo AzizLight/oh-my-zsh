a5340431c19a6d860239c7858e1f091e86e65afc
Aziz Light <aziiz.light+github@gmail.com>
2011-02-08 13:26:00 +0100

    Added an rvm alias and removed a bunch of useless ones

--- custom/plugins/rvm/rvm.plugin.zsh
+++ custom/plugins/rvm/rvm.plugin.zsh
@@ -3,15 +3,9 @@ fpath=($ZSH/plugins/rvm $fpath)
 autoload -U compinit
 compinit -i
 
-alias rvmi="rvm info"
-alias rl="rvm list"
-alias r192="rvm use 1.9.2"
-alias r191="rvm use 1.9.1"
-alias r186="rvm use 1.8.6"
-alias r187="rvm use 1.8.7"
-alias rd="rvm use default"
 alias rg="rvm gemset"
 alias rgl="rvm gemset list"
+alias rrc="rvm --rvmrc --create"
 
 # get the ruby name and version as well as the gemset
 # from https://github.com/jtzemp/oh-my-zsh/blob/deb452e823876e26bc91947fdd55f75873b02c1d/lib/rvm.zsh#L9
