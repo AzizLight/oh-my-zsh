ce903837f7a885aca3e2276a4c94fe71834c7b1d
Aziz Light <aziiz.light+github@gmail.com>
2011-02-09 13:53:55 +0100

    Added two new ruby aliases

--- custom/plugins/ruby/ruby.plugin.zsh
+++ custom/plugins/ruby/ruby.plugin.zsh
@@ -1,4 +1,9 @@
 alias r="ruby"
 
+# This is probably a temporary alias that
+# I will end up deleting later...
+alias rt="r test/test_*"
+
 # Fully replace irb by ripl
 alias irb="ripl"
+alias i="irb"
