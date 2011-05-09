07b82d9f47ef0295a7162e6ef71d8aeca30838f1
Aziz Light <aziz@azizlight.me>
2011-05-09 16:40:27 +0300

    Use MacVim's Vim

--- custom/plugins/vim/vim.plugin.zsh
+++ custom/plugins/vim/vim.plugin.zsh
@@ -1,3 +1,10 @@
+if [ -x `which brew` ]; then
+    macvim_version=`brew list --versions macvim | sed 's/.* \([0-9\.\-]*\)$/\1/'`
+    if [ -n "${macvim_version}" ]; then
+        alias vim=`brew --prefix`/Cellar/macvim/${macvim_version}/MacVim.app/Contents/MacOS/Vim
+    fi
+fi
+
 alias v="$VISUAL"
 
 alias v.="v ."
